2a8e56577318b8a814e408fdf20bd58a2c498e82
Mohamed <mohamedabuassaf@gmail.com>
2023-10-31 15:41:41 +0200

    new api request added for airquality

--- app/aq_apis.py
+++ app/aq_apis.py
@@ -55,8 +55,42 @@ class AllStations(Resource):
         cursor.close()
         return jsonify(result)
     
-@Airquality_apis.route('/api/stations/<stationid>')
+@Airquality_apis.route('/api/stations/<stationId>')
 class StationInfo(Resource):
     @api.doc(description='Get all info about a specific station we have in the database')
-    def get(self, stationdId):
-        pass
+    def get(self, stationId):
+        cursor = conn.cursor()
+        cursor.execute(f'SELECT * FROM airqualitystation WHERE stationid = {stationId}')
+        stationinfo = cursor.fetchone() ## get all info about the station in the database
+        
+        stationname = stationinfo[1] ## station name
+        stationserialnumber = stationinfo[2] ## serial number 
+        stationmodelid = stationinfo[3] ## station model ID
+        stationoperatorid = stationinfo[5] ## station operator ID 
+        
+        cursor = conn.cursor()
+        cursor.execute(f'SELECT stationlatitude, stationlongitude FROM stationlocation WHERE id = {stationId}')
+        longlat = cursor.fetchall() ## get the long lang for the current station
+        
+        cursor.execute(f'SELECT brandname FROM stationmodel WHERE id = {stationmodelid}')
+        model = cursor.fetchone()[0] ## get the brand name of the model
+        
+        
+        cursor.execute(f'SELECT operatorname, operatorweb, operatoremail FROM stationoperator WHERE id = {stationoperatorid}')
+        operator = cursor.fetchall() ## get the operator name
+            
+            
+        cursor.close()
+        return jsonify({
+            'id': stationId,
+            'name': stationname,## name
+            'serialNumber': stationserialnumber, ## serial number 
+            'model': model, # brand name of the model
+            'latitude': longlat[0][0], ## LATIDUTE
+            'longitude':longlat[0][1], ## LONGITUDE
+            'operator': operator[0][0], ## operator name
+            'operatorWeb': operator[0][1], ## operator website
+            'operatorEmail': operator[0][2], ## operator email
+            'contactEmail': '', ## empty for now
+            'contactPhone': '',
+        })
