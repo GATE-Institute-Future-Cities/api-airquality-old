bb5b32dd685f94f68d38635c6ecf337b8f2bb1c5
Mohamed <mohamedabuassaf@gmail.com>
2023-10-31 14:51:56 +0200

    new api request added for airquality

--- app/aq_apis.py
+++ app/aq_apis.py
@@ -14,25 +14,39 @@ class AllStations(Resource):
     @api.doc(description='Get all info about every station we have in the database')
     def get(self,):
         cursor = conn.cursor()
-        cursor.execute(f'SELECT stationid, stationname, stationserialnumber FROM airqualitystation')
+        cursor.execute(f'SELECT * FROM airqualitystation')
         all_stations = cursor.fetchall() ## get all the stations in the database
         result = []
 
         for station in all_stations:
             stationid = station[0] ## station id
+            stationname = station[1] ## station name
+            stationserialnumber = station[2] ## serial number 
+            stationmodelid = station[3] ## station model ID
+            stationoperatorid = station[5] ## station operator ID 
             
             cursor = conn.cursor()
             cursor.execute(f'SELECT stationlatitude, stationlongitude FROM stationlocation WHERE id = {stationid}')
             longlat = cursor.fetchall() ## get the long lang for the current station
             
+            cursor.execute(f'SELECT brandname FROM stationmodel WHERE id = {stationmodelid}')
+            model = cursor.fetchone()[0] ## get the brand name of the model
+            
+            
+            cursor.execute(f'SELECT operatorname, operatorweb, operatoremail FROM stationoperator WHERE id = {stationoperatorid}')
+            operator = cursor.fetchall() ## get the operator name
+            
+            
             record = {
                 'id': stationid,
-                'name': station[1],## station name
-                'latitude': longlat[0][0], ## getting the first lists element which is the LATIDUTE
-                'longitude':longlat[0][1], ## getting the first lists element which is the LONGITUDE
-                'serial number': station,
-                'model': station,
-                'location': station,
+                'name': stationname,## name
+                'serialNumber': stationserialnumber, ## serial number 
+                'model': model, # brand name of the model
+                'latitude': longlat[0][0], ## LATIDUTE
+                'longitude':longlat[0][1], ## LONGITUDE
+                'operator': operator[0][0], ## operator name
+                'operatorWeb': operator[0][1], ## operator website
+                'operatorEmail': operator[0][2] ## operator email
             }
             result.append(record)
 
