2fb0c5e1e3b4e8abba076a9d1da86d5de880fca2
Mohamed <mohamedabuassaf@gmail.com>
2023-11-27 15:54:52 +0200

    old apis

--- app/extensions.py
+++
@@ -1,7 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-from flask_restx import Api
-from flask_httpauth import HTTPDigestAuth
-
-api = Api(title='AirQuality')
-db = SQLAlchemy()
-auth = HTTPDigestAuth()
